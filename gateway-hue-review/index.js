8b740eec65e1ef02f748f960a545884acedf06c4
MoonTahoe <alex@moonhighway.com>
2019-10-26 10:33:17 -0700

    Initial Color Gateway

--- gateway-hue-review/index.js
+++ gateway-hue-review/index.js
@@ -1,1 +1,21 @@
-console.log("TODO: Build Hue Review Gateway");
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "users", url: "http://localhost:4000" },
+    { name: "colors", url: "http://localhost:4002" }
+  ]
+});
+
+const start = async () => {
+  const { schema, executor } = await gateway.load();
+  const server = new ApolloServer({ schema, executor });
+  server.listen(process.env.PORT).then(({ url }) => {
+    console.log(
+      `      🎨  🖍  👩‍💻  ✅   - The Hue Review Gateway API running at ${url}`
+    );
+  });
+};
+
+start();
